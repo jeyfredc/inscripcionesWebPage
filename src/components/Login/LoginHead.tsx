c435f166ac2caa8395cf34ca791b2dc2fac8920e
Jeyfredc <jeyfredc@gmail.com>
2025-06-04 09:51:12 -0500

    Fix error , importacion de logo

--- src/components/Login/LoginHead.tsx
+++ src/components/Login/LoginHead.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 interface LoginHeadProps {
     children: React.ReactNode
@@ -10,7 +11,7 @@ const LoginHead = ({children, title}: LoginHeadProps) => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-md">
       <div>
-          <img src="../public/logo.png" alt="Logo" className="w-32 mx-auto" />
+          <img src={logo} alt="Logo" className="w-32 mx-auto" />
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           {title}
         </h2>
