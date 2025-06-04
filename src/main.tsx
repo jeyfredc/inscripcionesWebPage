eb87ac064acaeab3cf95a704d3898b21bf58e0ad
Jeyfredc <jeyfredc@gmail.com>
2025-06-04 09:48:38 -0500

    Fix error, correccion error de extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRouter from './router.tsx'
+import AppRouter from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
