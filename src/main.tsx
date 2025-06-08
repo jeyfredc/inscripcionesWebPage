de042018e69e2979490ea6acb474b20ddd0f65dc
Jeyfredc <jeyfredc@gmail.com>
2025-06-08 15:37:00 -0500

    Se reemplaza hashRouter por BrowseRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter as BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './lib/axios';
 import AppRouter from './router';
